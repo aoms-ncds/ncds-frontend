5aa225d2689bb658817adaaf04b1172408bb0548
annmariyacomputervalley <105838096+annmariyacomputervalley@users.noreply.github.com>
2023-04-14 18:33:40 +0530

    update fr changes

---
+++ src/pages/FR/ManageFrPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CommonPageLayout from '../../components/CommonPageLayout';
+
+const ManageFrPage = () => {
+  return (
+    <CommonPageLayout title='Manage FR'>
+    </CommonPageLayout>
+  );
+};
+
+export default ManageFrPage;
