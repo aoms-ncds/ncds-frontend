003b0f9b067f04d1ec7e9e5c79cb762fb57bda3c
athirakharidas12 <103183703+athirakharidas12@users.noreply.github.com>
2023-04-14 20:06:52 +0530

    manage staff page design

--- src/pages/HR/ManagePage.tsx
+++ src/pages/HR/ManagePage.tsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CommonPageLayout from '../../components/CommonPageLayout';
+import { Button, Card, Grid, Link } from '@mui/material';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
+import HRServices from './extras/HRServices';
+import DropdownButton from '../../components/DropDownButton';
+import {
+  Edit as EditIcon,
+  Add as AddIcon,
+
+} from '@mui/icons-material';
 
 const HRManagePage = () => {
+  const [staffs, setStaffs] = useState<Staff[]>();
+  useEffect(() => {
+    HRServices.getStaffs()
+      .then((res) => {
+        console.log(res);
+        setStaffs(res.data);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  }, []);
+  const columns = [
+    {
+      field: '_manage',
+      headerName: 'Action',
+      minWidth: 50,
+      type: 'string',
+      renderCell: (props: any) => (
+        <DropdownButton
+          useIconButton={true}
+          id="attendance action"
+          primaryText="Actions"
+          key={'attendance action'}
+          items={[
+            {
+              id: 'edit',
+              text: 'Edit',
+              component: Link,
+              icon: EditIcon,
+              to: '/edit' + props.row._id,
+            },
+          ]}
+        />
+      ),
+    },
+    { field: '_id', headerName: 'id', width: 70 },
+    { field: 'Name', headerName: 'Name', width: 70 },
+    { field: 'DOB_DOJ', headerName: 'DOB, DOJ', width: 130 },
+    { field: 'Designation', headerName: 'Designation', width: 130 },
+    { field: 'Department', headerName: 'Department', width: 130 },
+    { field: 'Phone_email', headerName: 'Phone & email', width: 130 },
+    { field: 'Spouse_of_another_employee', headerName: 'Spouse_of_another_employee', width: 130 },
+    { field: 'ID_format', headerName: 'ID_format', width: 130 },
+  ];
   return (
-    <CommonPageLayout>
-      <div>hi</div>
+    <CommonPageLayout title='Manage Staff'>
+      <Button
+        variant="contained"
+        sx={{ float: 'right' }}
+        startIcon={<AddIcon />}
+        // onClick={() => {
+        // }}
+      >
+          Add new
+      </Button>
+      <br/><br/>
+      <Grid item xs={12} md={12}>
+        <Card style={{ height: '70vh', width: '100%' }}>
+          <DataGrid rows={staffs??[]} columns={columns} getRowId={(row) => row._id}/>
+        </Card>
+      </Grid>
     </CommonPageLayout>
   );
 };
---
+++ src/pages/HR/extras/HRTypes.d.ts
@@ -0,0 +1,13 @@
+export {};
+declare global{
+type Staff={
+    _id: string;
+    name: string;
+    DOB_DOJ: Date;
+    Designation: string;
+    Department: string;
+    Phone_email: string;
+    Spouse_of_another_employee: string;
+    ID_format: string;
+}
+}
