9d861df956ad793d951aa1a4d0423d11da0f100b
annmariyacomputervalley <105838096+annmariyacomputervalley@users.noreply.github.com>
2023-04-17 10:46:09 +0530

    fixed white color showing

--- src/pages/FR/components/FRCountCard.tsx
+++ src/pages/FR/components/FRCountCard.tsx
@@ -8,30 +8,30 @@ const FRCountCard = (props: {
     color: string;
 }) => {
   return (
-    <Card>
-      <CardActionArea
-        sx={{
-          'backgroundColor': props.color,
-          'color': 'white',
-          'borderRadius': 2,
-          'transition': 'all 0.3s',
-          'padding': 2,
-          '&:hover': {
-            boxShadow: '0 8px 16px 0 rgba(0, 0, 0, 0.6)',
-            transform: 'translate(0, -2px)',
-            cursor: 'pointer',
-          },
-          '&:active': {
-            boxShadow: 'none !important',
-            transform: 'translate(0, 2px)',
-            cursor: 'pointer',
-          },
-        }}>
-        <CardContent>
+    <Card sx={{
+      'backgroundColor': props.color,
+      'color': 'white',
+      'borderRadius': 3,
+      'transition': 'all 0.3s',
+      'padding': 2,
+      '&:hover': {
+        boxShadow: '0 8px 16px 0 rgba(0, 0, 0, 0.6)',
+        transform: 'translate(0, -2px)',
+        cursor: 'pointer',
+      },
+      '&:active': {
+        boxShadow: 'none !important',
+        transform: 'translate(0, 2px)',
+        cursor: 'pointer',
+      },
+    }}>
+      <CardActionArea>
+        <CardContent >
           <Typography variant='h5'>{props.count}</Typography>
           <Typography variant='h6' align="right">{props.secondaryText}</Typography>
         </CardContent>
       </CardActionArea>
+
 
     </Card>
   );
