dc17f33c81940f57f4524a553771027e51b5f816
Sanjaymon Sailas <107023168+sanjaymonsailas@users.noreply.github.com>
2023-04-19 14:33:27 +0530

    IRO Initial

---
+++ src/modules/IRO/ViewIRO.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CommonPageLayout from '../../components/CommonPageLayout';
+
+const ViewIRO = () => {
+  return (
+    <CommonPageLayout title='View and Manage IRO'>
+    </CommonPageLayout>
+  );
+};
+
+export default ViewIRO;
--- src/modules/IRO/index.tsx
+++ src/modules/IRO/index.tsx
@@ -1,10 +1,114 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CommonPageLayout from '../../components/CommonPageLayout';
+import { Grid, Card } from '@mui/material';
+import {
+  Edit as EditIcon,
+  Message as MessageIcon,
+  Preview as PreviewIcon,
+
+} from '@mui/icons-material';
+import PrintIcon from '@mui/icons-material/Print';
+import { DataGrid } from '@mui/x-data-grid';
+import { Link } from 'react-router-dom';
+import DropdownButton from '../../components/DropDownButton';
+import IROServices from './extras/IROServices';
 
 const IRODashboard = () => {
+  const [IROrder, setIROrder] = useState<IROrder[]>();
+  useEffect(() => {
+    IROServices.getAll()
+   .then((res) => {
+     console.log(res);
+     setIROrder(res.data);
+   })
+  .catch((res) => {
+    console.log(res);
+  });
+  }, []);
+  const columns = [
+    {
+      field: '_manage',
+      headerName: 'Action',
+      minWidth: 50,
+      type: 'string',
+      renderCell: (props: any) => (
+        <DropdownButton
+          useIconButton={true}
+          id='IRO action'
+          primaryText='Actions'
+          key={'IRO action'}
+          items={[
+            {
+              id: 'View',
+              text: 'View and Manage',
+              component: Link,
+              to: '/iro/view_IRO/' + props.row._id,
+              icon: PreviewIcon,
+            },
+            {
+              id: 'sendbackDivision',
+              text: 'Send Back to Division',
+              component: Link,
+              to: '/sendbackDivision' + props.row._id,
+              icon: PreviewIcon,
+            },
+            {
+              id: 'remarks',
+              text: 'Remarks',
+              component: Link,
+              to: '/iro/view_IRO/' + props.row._id,
+              icon: EditIcon,
+            },
+            {
+              id: 'print',
+              text: 'Print IRO',
+              component: Link,
+              to: '/view' + props.row._id,
+              icon: PrintIcon,
+            },
+            {
+              id: 'notification',
+              text: 'Send notification',
+              component: Link,
+              to: '/view' + props.row._id,
+              icon: MessageIcon,
+            },
+            {
+              id: 'edit',
+              text: 'Edit',
+              component: Link,
+              to: '/edit/:iroID' + props.row._id,
+              icon: EditIcon,
+            },
+            {
+              id: 'View',
+              text: 'View Details ',
+              component: Link,
+              to: '/iro/view_IRO/' + props.row._id,
+              icon: PreviewIcon,
+            },
+          ]}
+        />
+      ),
+    },
+    { field: '_id', headerName: 'SI No', width: 70 },
+    { field: 'IROno', headerName: 'IRO No', width: 70 },
+    { field: 'IROdate', headerName: 'IRO Date', width: 130 },
+    { field: 'Division_name', headerName: 'Division Name', width: 150 },
+    { field: 'Subdivision_name', headerName: 'Sub Division Name', width: 170 },
+    { field: 'Main_category', headerName: 'Main Category', width: 150 },
+    { field: 'Request_amount', headerName: 'Requested Amount', width: 130 },
+    { field: 'Lastupdate_date', headerName: 'Last Updated', width: 130 },
+    { field: 'Sanction', headerName: 'Special Sanction', width: 130 },
+  ];
   return (
-    <CommonPageLayout>
-      <div>IRODashboard</div>
+    <CommonPageLayout title='Internal Release Order'>
+      <br/><br/>
+      <Grid item xs={12} md={12}>
+        <Card style={{ height: '80vh', width: '100%' }}>
+          <DataGrid rows={IROrder??[]} columns={columns} getRowId={(row) => row._id}/>
+        </Card>
+      </Grid>
     </CommonPageLayout>
   );
 };
