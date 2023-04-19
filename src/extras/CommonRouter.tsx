9c1ff4d1ab77a07e2ef71789ca8ea649fcecd286
jishnu-cv <jishnu@computervalley.online>
2023-04-19 09:56:25 +0530

    Renamed pages=>modules

--- src/extras/CommonRouter.tsx
+++ src/extras/CommonRouter.tsx
@@ -4,12 +4,12 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import React from 'react';
-import homePageRoutes from '../pages/Home/extras/HomeRoutes';
-import divisionsPageRoutes from '../pages/Divisions/extras/DivisionsRoutes';
-import frPageRoutes from '../pages/FR/extras/FRRoutes';
-import hrPageRoutes from '../pages/HR/extras/HRRoutes';
-import iroPageRoutes from '../pages/IRO/extras/IRORoutes';
-import workersPageRoutes from '../pages/Workers/extras/WorkersRoutes';
+import homePageRoutes from '../modules/Home/extras/HomeRoutes';
+import divisionsPageRoutes from '../modules/Divisions/extras/DivisionsRoutes';
+import frPageRoutes from '../modules/FR/extras/FRRoutes';
+import hrPageRoutes from '../modules/HR/extras/HRRoutes';
+import iroPageRoutes from '../modules/IRO/extras/IRORoutes';
+import workersPageRoutes from '../modules/Workers/extras/WorkersRoutes';
 
 export const getAllModuleRoutes = [
   homePageRoutes,
