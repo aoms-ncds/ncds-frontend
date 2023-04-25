9d95afe34dc4d85dbcf5bed2bf1eb464ab90eefd
jishnu-cv <jishnu@computervalley.online>
2023-04-25 12:34:32 +0530

    Added GridLinkAction

--- src/modules/Sample/components/SampleTableComponent.tsx
+++ src/modules/Sample/components/SampleTableComponent.tsx
@@ -1,50 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { RefAttributes, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridActionsCellItem, GridActionsCellItemProps, GridRenderCellParams, GridRowParams } from '@mui/x-data-grid';
 import {
   Edit as EditIcon,
   Preview as PreviewIcon,
 } from '@mui/icons-material';
 import SampleServices from '../extras/SampleServices';
 import DropdownButton from '../../../components/DropDownButton';
+import GridLinkAction from '../../../components/GridLinkAction';
 
 interface SampleComponentProps {
   loadCount: number;
   onLoad: () => void;
   afterLoad: () => void;
 }
+
+
 const SampleComponent = (props: SampleComponentProps) => {
   const [sampleItems, setSampleItems] = useState<SampleItem[] | null>(null);
   const columns = [
     {
-      field: '_manage',
-      headerName: 'Action',
-      minWidth: 50,
-      type: 'string',
-      renderCell: (props: any) => (
-        <DropdownButton
-          useIconButton={true}
-          id='actions'
-          primaryText='Actions'
-          key={'actions'}
-          items={[
-            {
-              id: 'View',
-              text: 'View and Manage',
-              component: Link,
-              to: `/sample/${props.row._id}`,
-              icon: PreviewIcon,
-            },
-            {
-              id: 'edit',
-              text: 'Edit',
-              component: Link,
-              to: `/sample${props.row._id}`,
-              icon: EditIcon,
-            },
-          ]}
-        />
-      ),
+      field: 'actions',
+      type: 'actions',
+      getActions: (params: GridRowParams) => [
+        <GridLinkAction key={1} to={`/sample/${params.id}`} label='View' icon={<PreviewIcon />} showInMenu />,
+        <GridLinkAction key={2} to={`/sample/${params.id}/edit`} label='Edit' icon={<EditIcon />} showInMenu />,
+      ],
     },
     { field: 'name', headerName: 'Field 1', width: 400 },
     { field: 'email', headerName: 'Field 2', width: 400 },
