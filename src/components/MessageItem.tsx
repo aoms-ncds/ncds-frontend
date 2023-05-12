d4d8bb2ea2c54d0c564db62f7e5696a41d4f2c01
jishnu-cv <jishnu@computervalley.online>
2023-05-12 12:59:09 +0530

    LF -> CRLF

--- src/components/MessageItem.tsx
+++ src/components/MessageItem.tsx
@@ -1,12 +1,12 @@
 import { Alert, AlertTitle, Typography } from '@mui/material';
 import { Moment } from 'moment';
 import React from 'react';
-interface MessageItemProps{
-  key:string;
-    isSent: boolean;
-    body: string;
-    time: Moment;
-    sender: string;
+interface MessageItemProps {
+  key: string;
+  isSent: boolean;
+  body: string;
+  time: Moment;
+  sender: string;
 }
 const MessageItem = (props: MessageItemProps) => {
   return (
@@ -16,16 +16,20 @@ const MessageItem = (props: MessageItemProps) => {
       elevation={3}
       sx={{
         mb: 1,
-        [props.isSent ? 'mr':'ml']: 20,
+        [props.isSent ? 'mr' : 'ml']: 20,
         p: 1,
         pl: 5,
         borderRadius: props.isSent ? '20px 20px 20px 0' : '20px 20px 0 20px',
       }}
-      severity={props.isSent ? 'success':'info'}
+      severity={props.isSent ? 'success' : 'info'}
     >
-      <AlertTitle><u>{props.sender}</u></AlertTitle>
+      <AlertTitle>
+        <u>{props.sender}</u>
+      </AlertTitle>
       {props.body} <br />
-      <Typography variant='caption' sx={{ float: 'right', width: '100%' }}>{props.time.format('DD/MM/YYYY hh:mm A')}</Typography>
+      <Typography variant="caption" sx={{ float: 'right', width: '100%' }}>
+        {props.time.format('DD/MM/YYYY hh:mm A')}
+      </Typography>
     </Alert>
   );
 };
