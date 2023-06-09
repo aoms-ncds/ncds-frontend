f8422cd097bc5cb3916d221bd0c0aebd999ff50f
annmariyacomputervalley <105838096+annmariyacomputervalley@users.noreply.github.com>
2023-06-09 10:36:18 +0530

    display status

--- src/modules/FR/ManageFrPage.tsx
+++ src/modules/FR/ManageFrPage.tsx
@@ -22,6 +22,9 @@ import PrintIcon from '@mui/icons-material/Print';
 import SendIcon from '@mui/icons-material/Send';
 import MessageItem from '../../components/MessageItem';
 import { enqueueSnackbar } from 'notistack';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import FRreciptTemplate from './components/FRreciptTemplate';
+import FRLifeCycleStates from './extras/FRLifeCycleStates';
 
 const ManageFrPage = () => {
   const [FRRequests, setFRRequests] = useState<Frrequest[] | null>(null);
@@ -105,9 +108,9 @@ const ManageFrPage = () => {
             {
               id: 'print',
               text: 'Print FR',
-              component: Link,
-              to: '/view' + props.row._id,
-              icon: PrintIcon,
+              component: PDFDownloadLink,
+              document: <FRreciptTemplate />,
+              fileName: 'IFRReciept.pdf',
             },
             {
               id: 'notification',
@@ -158,6 +161,17 @@ const ManageFrPage = () => {
     { field: 'sanctionedAsPer', headerName: 'Special Sanction', renderCell: (props: any) => (
       <p> {props.row.sanctionedAsPer}</p>
     ), width: 150, align: 'center', headerAlign: 'center' },
+    { field: 'status', headerName: 'Status', renderCell: (props: any) => (
+      <p> {
+        props.row.status===FRLifeCycleStates.FR_APPROVED?'FR Approved':
+          props.row.status===FRLifeCycleStates.SUBMITTED_TO_PRESIDENT?'Submitted To President':
+            props.row.status===FRLifeCycleStates.SUBMITTED_TO_ACCOUNTS?'Submitted To Accounts':
+              props.row.status===FRLifeCycleStates.PRESIDENT_APPROVED?'President Approved':
+                props.row.status===FRLifeCycleStates.ACCOUNTS_APPROVED?'Accounts Approved':
+                  props.row.status===FRLifeCycleStates.FR_CREATED?'Created':''
+
+      } </p>
+    ), width: 130 },
   ];
 
   return (
