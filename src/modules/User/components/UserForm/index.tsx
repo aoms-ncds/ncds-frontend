eaba06f7b494e06b4fe0de2e1091bedf422779a0
TittuThomasGeorgy <117733012+TittuThomasGeorgy@users.noreply.github.com>
2023-06-13 13:15:52 +0530

    Datepicker Format

--- src/modules/User/components/UserForm/index.tsx
+++ src/modules/User/components/UserForm/index.tsx
@@ -547,6 +547,7 @@ const UserForm = <UserType extends CreatableStaff | CreatableIWorker>(
                     <DatePicker
                       label="Date Of Birth"
                       value={newChild?.dateOfBirth}
+                      format="DD/MM/YYYY"
                       onChange={(date: Moment | null) => {
                         if (date) {
                           setNewChild((newChild) => ({
