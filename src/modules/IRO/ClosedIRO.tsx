ee447949254620eb3f6aed915417f51aeca44a2e
5eshumadhavan <seshumadhavan2000@gmail.com>
2023-07-04 17:19:37 +0530

    Fixing the alignment of closed FR and IRO Fixes #211

--- src/modules/IRO/ClosedIRO.tsx
+++ src/modules/IRO/ClosedIRO.tsx
@@ -76,16 +76,15 @@ const ClosedIRO = () => {
         />
       ),
     },
-    { field: '_id', headerName: 'SI No', width: 70 },
     { field: 'IROno', headerName: 'IRO No', width: 70 },
     { field: 'IROdate', headerName: 'IRO Date', width: 130 },
     { field: 'divisionName', headerName: 'Division Name', width: 150 },
     { field: 'subDivisionName', headerName: 'Sub Division Name', width: 170 },
     { field: 'mainCategory', headerName: 'Main Category', width: 150 },
-    { field: 'requestAmount', headerName: 'Requested Amount', width: 130 },
+    { field: 'requestAmount', headerName: 'Requested Amount', width: 150 },
     { field: 'lastUpdateDate', headerName: 'Last Updated', width: 130 },
     { field: 'sanction', headerName: 'Special Sanction', width: 130 },
-    { field: 'sanctionedAmount', headerName: 'Sanctioned Amount', width: 130 },
+    { field: 'sanctionedAmount', headerName: 'Sanctioned Amount', width: 150 },
     { field: 'sanctionedAsPer', headerName: 'Sanctioned As Per', width: 130 },
     { field: 'sourceBank', headerName: 'Source Bank', width: 130 },
   ];
