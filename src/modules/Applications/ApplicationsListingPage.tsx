17d1647456b695990e3c3710da1f2c93d3ddb413
HS419MADHU <86403245+HS419MADHU@users.noreply.github.com>
2023-07-31 18:40:13 +0530

    adding the multiline textbox for application reason and fixing its view Fixes #332

--- src/modules/Applications/ApplicationsListingPage.tsx
+++ src/modules/Applications/ApplicationsListingPage.tsx
@@ -118,7 +118,6 @@ const ApplicationsListingPage = (props: { action: 'manage' | 'hr' | 'president' 
         });
     }
   }, []);
-
   const EditApplication = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (editid) {
@@ -318,7 +317,15 @@ const ApplicationsListingPage = (props: { action: 'manage' | 'hr' | 'president' 
     { field: 'name', align: 'center',
       headerAlign: 'center', renderHeader: () => (<b>Name</b>), width: 150 },
     { field: 'reason', align: 'center',
-      headerAlign: 'center', renderHeader: () => (<b>Reason</b>), width: 150 },
+      headerAlign: 'center', renderHeader: () => (<b>Reason</b>),
+      renderCell: (params) => (
+        <p style={{ maxWidth: 'auto',
+          whiteSpace: 'normal',
+          wordBreak: 'break-word' }}>
+          {params.value}
+        </p>),
+      width: 250,
+    },
     {
       field: 'createdBy', renderHeader: () => (<b>Applied By</b>), renderCell: (props) =>
         <p> {props.row.createdBy?.basicDetails.firstName + ' ' + props.row.createdBy?.basicDetails.lastName}</p>,
@@ -397,6 +404,7 @@ const ApplicationsListingPage = (props: { action: 'manage' | 'hr' | 'president' 
                       }));
                     }}
                     fullWidth
+                    multiline
                     required
                   />
                 </Grid>
