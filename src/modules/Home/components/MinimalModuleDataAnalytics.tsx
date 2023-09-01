e91163dc6941c00cba639fd4c05ad9da91351ffb
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2023-09-01 12:02:02 +0530

    ui changes

--- src/modules/Home/components/MinimalModuleDataAnalytics.tsx
+++ src/modules/Home/components/MinimalModuleDataAnalytics.tsx
@@ -104,7 +104,7 @@ const MinimalModuleDataAnalytics = () => {
                 secondaryText='Divisions'
                 count={divisionsCount?.toString()}
                 color={'#75C2F6'}
-                targetRoute={`/divisions/details/${(user.user as User).division}`}
+                targetRoute="/divisions/"
                 // icon={<NotificationsIcon color="secondary" sx={{ fontSize: 70 }} />}
                 icon={<img src="/divisionLogo.png" alt="Logo" style={{ width: '70px', height: '70px' }} />}
               />
@@ -113,8 +113,8 @@ const MinimalModuleDataAnalytics = () => {
             <Grid item xs={6} md={3} xl={4}>
 
               <DashBoardCard secondaryText='Division'
-                icon={<img src="/divisionLogo.png" alt="Logo" style={{ width: '70px', height: '70px' }} />} count={curDivision?.toString()}
-                color="#75C2F6" targetRoute="/divisions/" />
+                icon={<img src="/divisionLogo.png" alt="Logo" style={{ width: '70px', height: '70px' }} />}  count={divisionsCount?.toString()}
+                color="#75C2F6" targetRoute={`/divisions/details/${(user.user as User).division}`} />
 
             </Grid>
           }
@@ -134,7 +134,7 @@ const MinimalModuleDataAnalytics = () => {
 
             </Grid>: <Grid item xs={6} md={3} xl={4}>
               <DashBoardCard secondaryText='Sub-Divisions' icon={<img src="/divisionLogo.png" alt="Logo" style={{ width: '70px', height: '70px' }} />}
-                count={subDivisionsCount?.toString()} color="#90021f" targetRoute={`/divisions/details/${(user.user as User).division}`} />
+                count={subDivisionsCount?.toString()} color="#75C2F6" targetRoute={`/divisions/details/${(user.user as User).division}`} />
             </Grid>
 
           }
