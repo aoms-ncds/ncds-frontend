a4460323805c530181c3674d1086a7a37d08151e
TittuThomasGeorgy <117733012+TittuThomasGeorgy@users.noreply.github.com>
2023-11-20 21:17:54 +0530

    Users / Support : Add a new text field "Other Designation" Fixes #473

--- src/modules/User/Profile.tsx
+++ src/modules/User/Profile.tsx
@@ -315,6 +315,7 @@ const Profile = () => {
           ) : 2}>
             <Grid container spacing={3}>
               <Grid item xs={12} lg={4}> <Typography variant='body1' component='span' sx={{ fontWeight: 'bolder' }}>Designation: </Typography> {user?.supportDetails?.designation?.name} </Grid>
+              <Grid item xs={12} lg={4}> <Typography variant='body1' component='span' sx={{ fontWeight: 'bolder' }}>Other Designation: </Typography> {user?.supportDetails?.otherDesignation} </Grid>
               <Grid item xs={12} lg={4}> <Typography variant='body1' component='span' sx={{ fontWeight: 'bolder' }}>Department: </Typography> {user?.supportDetails?.department?.name} </Grid>
               <Grid item xs={12} lg={4}> <Typography variant='body1' component='span' sx={{ fontWeight: 'bolder' }}>Total No of years in Ministry: </Typography> {user?.supportDetails?.totalNoOfYearsInMinistry} </Grid>
               <Grid item xs={12} lg={4}> <Typography variant='body1' component='span' sx={{ fontWeight: 'bolder' }}>With Church: </Typography> {user?.supportDetails?.withChurch ? 'Yes' : (user?.supportDetails?.withChurch === false ? 'No' : 'Not specified')} </Grid>
