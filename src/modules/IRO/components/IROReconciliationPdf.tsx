aeae32847d49a30e2f5d4045b044a167f046bc2a
shibinsk <shibinskombanal@gmail.com>
2024-05-09 18:26:41 +0530

    Revert "Reapply "Signature sheet Modification""
    This reverts commit d331964869b4413fa061d6a0ffdec93956a2754d.

--- src/modules/IRO/components/IROReconciliationPdf.tsx
+++ src/modules/IRO/components/IROReconciliationPdf.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     marginTop: 65,
     fontSize: 14,
     position: 'absolute',
-    left: 150,
+    left: 160,
     color: 'darkblue',
   },
 
@@ -73,8 +73,6 @@ const styles = StyleSheet.create({
     padding: 2,
     textAlign: 'center',
     fontWeight: 'bold',
-    fontFamily: 'Oswald',
-
   },
   tableCell: {
     flex: 1,
