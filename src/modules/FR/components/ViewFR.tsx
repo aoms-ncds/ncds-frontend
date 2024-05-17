893f51576f91ddca2e70d972d1a2f0845dede644
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2024-05-17 16:57:17 +0530

    miner issue fix

--- src/modules/FR/components/ViewFR.tsx
+++ src/modules/FR/components/ViewFR.tsx
@@ -93,7 +93,7 @@ const ViewFRRequests = (props: FormComponentProps<CreatableFR, { FRLoaded: boole
   const [open, setOpen] = useState(false);
   const [open2, setOpen2] = useState(false);
   const [divisions, setDivisions] = useState<Division | null>(null);
-  console.log(newParticular, 'newParticular');
+  console.log(props, 'newParticular');
 
 
   let total = 0;
@@ -498,20 +498,20 @@ const ViewFRRequests = (props: FormComponentProps<CreatableFR, { FRLoaded: boole
                         }
 
                       >
-                        {divisions?.DivisionBankFCRA?.bankName? <MenuItem value={divisions?.FCRABankDetails?.bankName ? 'FCRA' : 'Division Bank FCRA'}>Division Bank FCRA - {divisions?.DivisionBankFCRA?.beneficiary}</MenuItem> : '' }
-                        {divisions?.DivisionBankLocal?.bankName? <MenuItem value={divisions.localBankDetails?.bankName? 'Local Bank' :'Division Bank Local'}>Division Bank Local - {divisions?.DivisionBankLocal?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank1?.bankName? <MenuItem value={ 'Beneficiary Bank 1'}>Beneficiary Bank 1 - {divisions?.BeneficiaryBank1?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank2?.bankName? <MenuItem value={'Beneficiary Bank 2'}>Beneficiary Bank 2 - {divisions?.BeneficiaryBank2?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank3?.bankName? <MenuItem value={'Beneficiary Bank 3'}>Beneficiary Bank 3 - {divisions?.BeneficiaryBank3?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank4?.bankName? <MenuItem value={'Beneficiary Bank 4'}>Beneficiary Bank 4 - {divisions?.BeneficiaryBank4?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank5?.bankName? <MenuItem value={'Beneficiary Bank 5'}>Beneficiary Bank 5 - {divisions?.BeneficiaryBank5?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank6?.bankName? <MenuItem value={'Beneficiary Bank 6'}>Beneficiary Bank 6 - {divisions?.BeneficiaryBank6?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank7?.bankName? <MenuItem value={'Beneficiary Bank 7'}>Beneficiary Bank 7 - {divisions?.BeneficiaryBank7?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank8?.bankName? <MenuItem value={'Beneficiary Bank 8'}>Beneficiary Bank 8 - {divisions?.BeneficiaryBank8?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank9?.bankName? <MenuItem value={'Beneficiary Bank 9'}>Beneficiary Bank 9 - {divisions?.BeneficiaryBank9?.beneficiary}</MenuItem> :'' }
-                        {divisions?.BeneficiaryBank10?.bankName? <MenuItem value={'Beneficiary Bank 10'}>Beneficiary Bank 10 - {divisions?.BeneficiaryBank10?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.DivisionBankFCRA?.bankName || props?.value?.division?.FCRABankDetails?.bankName ? <MenuItem value={props.value.division?.FCRABankDetails?.bankName ? 'FCRA' : 'Division Bank FCRA'}>Division Bank FCRA - {props.value.division?.DivisionBankFCRA?.beneficiary || props.value.division?.FCRABankDetails?.beneficiary}</MenuItem> : '' }
+                        {props?.value?.division?.DivisionBankLocal?.bankName || props?.value?.division?.localBankDetails?.bankName ? <MenuItem value={props.value.division.localBankDetails?.bankName? 'Local Bank' :'Division Bank Local'}>Division Bank Local - {props.value.division?.DivisionBankLocal?.beneficiary || props.value.division?.localBankDetails?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank1?.bankName || props?.value?.division?.otherBankDetails?.bankName? <MenuItem value={'Beneficiary Bank 1'}>Beneficiary Bank 1 - {props.value.division?.BeneficiaryBank1?.beneficiary || props.value.division?.localBankDetails?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank2?.bankName? <MenuItem value={'Beneficiary Bank 2'}>Beneficiary Bank 2 - {props.value.division?.BeneficiaryBank2?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank3?.bankName? <MenuItem value={'Beneficiary Bank 3'}>Beneficiary Bank 3 - {props.value.division?.BeneficiaryBank3?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank4?.bankName? <MenuItem value={'Beneficiary Bank 4'}>Beneficiary Bank 4 - {props.value.division?.BeneficiaryBank4?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank5?.bankName? <MenuItem value={'Beneficiary Bank 5'}>Beneficiary Bank 5 - {props.value.division?.BeneficiaryBank5?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank6?.bankName? <MenuItem value={'Beneficiary Bank 6'}>Beneficiary Bank 6 - {props.value.division?.BeneficiaryBank6?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank7?.bankName? <MenuItem value={'Beneficiary Bank 7'}>Beneficiary Bank 7 - {props.value.division?.BeneficiaryBank7?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank8?.bankName? <MenuItem value={'Beneficiary Bank 8'}>Beneficiary Bank 8 - {props.value.division?.BeneficiaryBank8?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank9?.bankName? <MenuItem value={'Beneficiary Bank 9'}>Beneficiary Bank 9 - {props.value.division?.BeneficiaryBank9?.beneficiary}</MenuItem> :'' }
+                        {props?.value?.division?.BeneficiaryBank10?.bankName? <MenuItem value={'Beneficiary Bank 10'}>Beneficiary Bank 10 - {props.value.division?.BeneficiaryBank10?.beneficiary}</MenuItem> :'' }
 
-                        {/* <MenuItem value={'Beneficiary Bank 2'}>Beneficiary Bank 2 - {divisions?.BeneficiaryBank2?.beneficiary}</MenuItem>
+                        {/* <MenuItem value={'Beneficiary Bank 2'}>Beneficiary Bank 2 - {props.value.division?.BeneficiaryBank2?.beneficiary}</MenuItem>
                         <MenuItem value={'Beneficiary Bank 3'}>Beneficiary Bank 3 - {divisions?.BeneficiaryBank3?.beneficiary}</MenuItem>
                         <MenuItem value={'Beneficiary Bank 4'}>Beneficiary Bank 4 - {divisions?.BeneficiaryBank4?.beneficiary}</MenuItem>
                         <MenuItem value={'Beneficiary Bank 5'}>Beneficiary Bank 5 - {divisions?.BeneficiaryBank5?.beneficiary}</MenuItem>
