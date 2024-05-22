9e00a5bc5b55c40365c2253b4d549288c3c6dbd8
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2024-05-22 14:55:38 +0530

    ui

--- src/modules/FR/components/FRReceiptTemplate.tsx
+++ src/modules/FR/components/FRReceiptTemplate.tsx
@@ -242,7 +242,7 @@ const FRReceiptTemplate = (props: { rowData: FR; president: EsignaturePresident 
           {props.rowData?.specialsanction == 'Yes' ? (
             <Image style={{
               left: 60,
-              top: 90,
+              top: 150,
               position: 'absolute',
               height: 35,
               width: 50,
@@ -255,8 +255,8 @@ const FRReceiptTemplate = (props: { rowData: FR; president: EsignaturePresident 
             left: 60,
             top: 120,
             position: 'absolute',
-            height: 135,
-            width: 150,
+            height: 35,
+            width: 50,
           }}
           src={`data:${props.rowData?.additionalSignature?.type};base64, ${props.rowData?.additionalSignature?.base64} `} />
         </div>
@@ -275,8 +275,8 @@ const FRReceiptTemplate = (props: { rowData: FR; president: EsignaturePresident 
           <Text style={{ left: 460, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>{props.rowData?.division?.details.juniorLeader?.name?.basicDetails?.firstName} {props.rowData?.division?.details.juniorLeader?.name?.basicDetails?.lastName}</Text>
           {props.rowData?.specialsanction == 'Yes' ? (
             <>
-              <Text style={{ left: 60, top: 90, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>President</Text>
-              <Text style={{ left: 60, top: 70, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>{props.rowData?.division?.details.president?.name?.basicDetails?.firstName} {props.rowData?.division?.details.president?.name?.basicDetails?.lastName}</Text>
+              <Text style={{ left: 60, top: 100, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>President</Text>
+              <Text style={{ left: 60, top: 90, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>{props.rowData?.division?.details.president?.name?.basicDetails?.firstName} {props.rowData?.division?.details.president?.name?.basicDetails?.lastName}</Text>
 
               <Text style={{ left: 60, top: 120, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>President Approved Date :</Text>
               <Text style={{ left: 165, top: 120, position: 'absolute', fontSize: 10, fontWeight: 'bold', fontFamily: 'Oswald' }}>
