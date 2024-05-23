8e7c0bb36b8b99511dd159b5463f91dd125d8784
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2024-05-23 16:39:02 +0530

    ERR

--- src/modules/IRO/components/IROReconciliationPdf.tsx
+++ src/modules/IRO/components/IROReconciliationPdf.tsx
@@ -262,7 +262,7 @@ const IROReconciliationPdf = (props:{data:
         (row.supportStructure?.MUTDeduction ?? 0)
       ):0}</Text>
               <div style={styles.cellGrid}></div>
-              <Text style={{ ...styles.tableCell, fontWeight: 'bold' }}>{total}</Text>
+              <Text style={{ ...styles.tableCell, fontWeight: 'bold' }}></Text>
               <div style={styles.cellGrid}></div>
             </View>
           </>
