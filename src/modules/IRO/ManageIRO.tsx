4bffab6c2557064e9d62e19d4a8fb5312034a5f9
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2024-06-06 14:20:04 +0530

    division filter

--- src/modules/IRO/ManageIRO.tsx
+++ src/modules/IRO/ManageIRO.tsx
@@ -1029,7 +1029,8 @@ const ManageIRO = (props: { action: 'manage' | 'release' }) => {
      (row.IRODate && row.IRODate.format('DD/MM/YYYY').toLowerCase().includes(searchText.toLowerCase())) ||
      (row.particulars[0]?.subCategory1 && row.particulars[0]?.subCategory1.toLowerCase().includes(searchText.toLowerCase())) ||
      (row.particulars[0]?.subCategory2 && row.particulars[0]?.subCategory2.toLowerCase().includes(searchText.toLowerCase())) ||
-     (row.particulars[0]?.subCategory3 && row.particulars[0]?.subCategory3.toLowerCase().includes(searchText.toLowerCase()))
+     (row.particulars[0]?.subCategory3 && row.particulars[0]?.subCategory3.toLowerCase().includes(searchText.toLowerCase())) ||
+     (row.division?.details.name && row.division?.details.name.toLowerCase().includes(searchText.toLowerCase()))
     ) {
       return true;
     }
