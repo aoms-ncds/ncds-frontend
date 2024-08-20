7e8c98f6c98239c9536638ad30675d5ffaf0296e
Shibin Sk <109890759+ShibinSk@users.noreply.github.com>
2024-08-20 10:26:29 +0530

    child quantity updated

--- src/modules/FR/ChildeSupprt.tsx
+++ src/modules/FR/ChildeSupprt.tsx
@@ -81,6 +81,8 @@ const ChildeSupportPage = () => {
   const [pdfProps, setPdfProps] = useState<{ divisionId: string | null; childId: string | null }>({ divisionId: null, childId: null });
   const [fileObj, setFileObj] = useState<FileObject | null>(null);
   const [loading, setLoading] = useState<boolean | null>(false);
+  // const supportEnabledWorkers = childList?.filter((item) => item.supportStructure?.supportEnabled === true);
+
   const addFR = async (requisition: CreatableFR) => {
     try {
       // const snackbarId =
@@ -422,7 +424,7 @@ const ChildeSupportPage = () => {
               narration: 'Towards the Monthly Support of <DESIGNATION NAME> Mr/Ms/Mrs <NAME>for the month of <MONTH, YEAR>',
               requestedAmount: total,
               unitPrice: total,
-              // quantity: supportEnabledWorkers?.length,
+              quantity: childList?.length,
               attachment: fileObj ? [fileObj] : [],
             }],
           }));
