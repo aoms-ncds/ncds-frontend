93828f8b5bba2454f673feda95be284ac93c5c6a
Merin Sunny <merinsunnyovelil@gmail.com>
2026-01-25 10:13:23 +0530

    updated new backend url

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import CommonHelpers from './extras/CommonHelpers';
 import Router from './extras/CommonRouter';
 import { useLoader } from './hooks/Loader';
 
-const serverURL = 'http://localhost:8002';
- //const serverURL = 'https://iet-staging.apis.pro910.app/';
-//const serverURL = 'https://iet-apis.pro910.app';
-
+// const serverURL = 'http://localhost:8002';
+// const serverURL = 'https://iet-staging.apis.pro910.app/';
+// const serverURLOld = 'https://iet-apis.pro910.app';//old
+const serverURL = 'https://iet-apis.ietapps.org';// new
 axios.defaults.baseURL = serverURL;
 // window.location.hostname === 'localhost' ? 'http://localhost:8002' : 'https://iet-apis.pro910.app';
 
