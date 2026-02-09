ff3d7883521ea3ef9ed7368001798fd4272dca52
Merin Sunny <merinsunnyovelil@gmail.com>
2026-02-09 14:19:06 +0530

    replace exiting recordes in the db url iet.pro910.com to aoms

--- src/modules/Notification/components/MessageContent.tsx
+++ src/modules/Notification/components/MessageContent.tsx
@@ -39,8 +39,10 @@ function MessageContent() {
             </Typography>
             <br />
             {message?.ref_url && (
-              <Typography variant='body1' >
-                <Link to={message?.ref_url}> {message?.ref_url}</Link>
+              <Typography variant="body1">
+                <Link to={message?.ref_url?.replace('iet.pro910.com', 'aoms.ietapps.org')}>
+                  {message?.ref_url?.replace('iet.pro910.com', 'aoms.ietapps.org')}
+                </Link>
               </Typography>
             )}
             <br />
