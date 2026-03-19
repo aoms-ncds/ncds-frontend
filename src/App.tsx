38291c80225a80ce87e153a4e46aa3ba34c241ab
Shibin Sk <shibinskombanal@gmail.com>
2026-03-19 12:33:21 +0530

    ui

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ import CommonHelpers from './extras/CommonHelpers';
 import Router from './extras/CommonRouter';
 import { useLoader } from './hooks/Loader';
 
-const serverURL = 'http://localhost:8002';
+// const serverURL = 'http://localhost:8002';
 // const serverURL = 'https://iet-staging.apis.pro910.app/';
 // const serverURLOld = 'https://iet-apis.pro910.app';//old
- const serverURL = 'https://iet-apis.ietapps.org';// neww
+
 
 const serverURL = 'https://iet-apis.ietapps.org';// neww
 
