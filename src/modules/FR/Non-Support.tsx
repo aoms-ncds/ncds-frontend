c4b4643ee171f627d7aaa4a4f0ff9ad3e515d406
Shibin Sk <shibinskombanal@gmail.com>
2026-04-16 16:57:46 +0530

    ui

--- src/modules/FR/Non-Support.tsx
+++ src/modules/FR/Non-Support.tsx
@@ -512,62 +512,7 @@ const NonSupport = () => {
     //   align: 'center',
     //   headerAlign: 'center',
     // },
-    {
-      field: 'status',
-      headerClassName: 'super-app-theme--cell',
-      renderHeader: () => <b>Status</b>,
-      cellClassName: (params) => {
-        console.log('CellClassName params:', params);
-        const statusName = params.formattedValue;
-        console.log('Status Name###:', statusName);
-        if (params.value == null) {
-          return '';
-        }
-        switch (statusName) {
-        case 'REVERTED':
-          return clsx('red-light');
-        case 'WAITING FOR ACCOUNTS':
-          return clsx('orange');
-        case 'IRO CLOSED':
-          return clsx('green');
-        case 'FR VERIFIED':
-          return clsx('green');
-        case 'FR CLOSED':
-          return clsx('green');
-        case ' FR_REJECTED':
-          return clsx('red');
-        case 'WAITING FOR PRESIDENT':
-          return clsx('orange');
-        default:
-          console.log('No class applied');
-          return '';
-        }
-      },
-      width: 205,
-      align: 'center',
-      headerAlign: 'center',
-      valueGetter: (params) => {
-        let statusName = IROLifeCycleStates.getStatusNameByCodeTransaction(params.value);
-        console.log(statusName, 'lolpß');
-        // Check if the status name needs to be changed
-        switch (statusName) {
-        case 'SEND_BACK':
-          statusName = 'REVERTED';
-          break;
-        case 'FR_APPROVED':
-          statusName = 'FR VERIFIED'; // Change to whatever new name you want
-          break;
-        case 'FR_REJECTED':
-          statusName = ' FR DISAPPROVED'; // Change to whatever new name you want
-          break;
-          // Add more cases for other status names you want to change
-        default:
-          statusName = statusName.replaceAll('_', ' ');
-          break;
-        }
-        return statusName;
-      },
-    },
+   
     {
       field: 'divisionName',
       headerClassName: 'super-app-theme--cell',
